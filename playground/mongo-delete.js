0255272db51ccbf401c115117a203c2045093679
surwase <pratap7684@gmail.com>
2017-07-05 13:15:06 +0530

    delete data

--- playground/mongo-delete.js
+++ playground/mongo-delete.js
@@ -5,9 +5,13 @@ MongoClient.connect('mongodb://localhost:27017/TodoApi' , (error ,  db)  => {
   if (error) {
     return console.log('Unable to connect DB Server');
   }
-  console.log('Connected  to MongoDb Server');
-db.collection('Todos').deleteMany({result: true}).then((result)  => {
+//   console.log('Connected  to MongoDb Server');
+// db.collection('Todos').deleteOne({text: 'Today is clean sky'}).then((result)  => {
+//   console.log(result);
+// });
+
+db.collection('Users').findOneAndDelete({age: 40}).then((result) => {
   console.log(result);
-});
+})
     db.close();
 } );
